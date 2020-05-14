d0955cfba15dc51eec0d47505d5b81e20878d14b
arpitsingh <47824144+arpit282@users.noreply.github.com>
2020-05-14 20:35:57 +0530

    Update intellify.R

--- intellify.R
+++ intellify.R
@@ -1,61 +1,7 @@
 ###########################################################################
-###############          Logistic regression                 ###########
-
-
-setwd("E:/R (Data Science)/excel files")
-
-
-data <- read.csv("binary.csv")
-
-str(data)
-
-data$admit <- as.factor(data$admit)
-data$rank <- as.factor(data$rank)
-
-
-############ training and testing data #############
-set.seed(123)
-id <- sample(2,nrow(data),replace = T,prob = c(.8,.2))
-
-train <- data[id == 1, ]
-test <- data[id ==2,]
-
-
-##### Model ######
-
-model <-glm(admit ~ gpa+rank,train,family = "binomial")
-
-summary(model)
-
-# prediction training dataset
-p1<-predict(model,train,type="response")
-head(p1)
-head(train)
-
-# misclassification train
-pred1 <- ifelse(p1>.5,1,0)
-tab1 <- table(prediction=pred1,actual=train$admit)
-tab1
-
-1-sum(diag(tab1))/sum(tab1)
-
-
-# misclassification on test dataset
-p2 <-predict(model,test,type="response")
-head(p2)
-head(test)
-
-
-pred2 <- ifelse(p2>.5,1,0)
-tab2 <- table(predication =pred2,actual = test$admit)
-tab2
-
-
-1-sum(diag(tab2))/sum(tab2)
-
-
 #######################  K-Nearest Neighbour (KNN)  #####################3
 
+setwd("E:/R (Data Science)/excel files")     ######  uses your working directory
 
 data <- read.csv("Prostate_Cancer.csv")
 
